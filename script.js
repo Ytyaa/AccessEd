050b9827363901d8d9695cd51778f45dd5d8a4f6
adam <adamkertiev2004@gmail.com>
2025-11-20 03:08:43 +0300

    [add / edit] ai, anim / style

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const aside = document.querySelector('.aside');
-const toggleBtn = document.getElementById('toggleAside');
+const aside = document.querySelector(".aside");
+const toggleBtn = document.getElementById("toggleAside");
+const asideBtn = document.querySelectorAll(".aside-button");
 
-toggleBtn.addEventListener('click', () => {
-    aside.classList.toggle('collapsed');
+toggleBtn.addEventListener("click", () => {
+  aside.classList.toggle("collapsed");
 });
 
-
+asideBtn.forEach((asBtn) => {
+  asBtn.addEventListener("click", () => {
+    const asideButtonActive = document.querySelector(".aside-button--active");
+    if (asideButtonActive) {
+      asideButtonActive.classList.remove("aside-button--active");
+    }
+    asBtn.classList.add("aside-button--active");
+  });
+});
